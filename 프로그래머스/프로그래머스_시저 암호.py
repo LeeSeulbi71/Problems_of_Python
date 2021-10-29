02f43561be07e4ac59ef5138917985051fc00aa0
LeeSeulbi71 <dltmfql01@seoultech.ac.kr>
2021-10-29 10:33:07 +0900

    연습문제

--- 프로그래머스/프로그래머스_시저 암호.py
+++ 프로그래머스/프로그래머스_시저 암호.py
@@ -4,12 +4,17 @@ def solution(s,n):
     s = list(s)
     new_s = []
     for i in s:
-        if ord(i) == 32:
+        to_number = ord(i)
+        if to_number == 32:
             new_s.append(" ")
+        elif 91<= (to_number+n) <=96:
+            to_number = 64 + n
+            new_s.append(chr(to_number))
+        elif (to_number+n) >= 123:
+            to_number = 96+n
+            new_s.append(chr(to_number))
         else:
-            if (ord(i) + n) > 91:
-                (ord(i) + n) 
-            new_s.append(chr(ord(i) + n))
+            new_s.append(chr(to_number+n))
     return "".join(new_s)
 
-print(solution("z", 1))
+print(solution("a B z", 4))
