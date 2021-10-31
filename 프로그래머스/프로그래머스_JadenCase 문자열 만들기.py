462958a198290ece84710e1e7b7e769a199be00f
LeeSeulbi71 <dltmfql01@seoultech.ac.kr>
2021-10-31 22:44:28 +0900

    연습문제

---
+++ 프로그래머스/프로그래머스_JadenCase 문자열 만들기.py
@@ -0,0 +1,17 @@
+#Split에 따라 나누기
+#만약 첫 글자가 영문이면 upper()
+#아니면 나머지 전부 다 lower()
+#" ".join(리스트)
+
+def solution(s):
+    JadenCase = list(s.split())
+
+    for i in range(len(JadenCase)):
+        if JadenCase[i][0].isalpha():
+            JadenCase[i] = JadenCase[i][0].upper() + JadenCase[i][1:].lower()
+        else:
+            JadenCase[i] = JadenCase[i][0] + JadenCase[i][1:].lower()
+    
+    return " ".join(JadenCase)
+
+print(solution("for the last week"))
