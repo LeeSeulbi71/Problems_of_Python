7f0a7825a94bd231bd0e5518dd00cd095aac7aca
LeeSeulbi71 <dltmfql01@seoultech.ac.kr>
2021-11-03 22:51:06 +0900

    Chapter5

---
+++ 웹크롤링/Chapter5/1. 셀레니움 기초 사용법.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+
+# 브라우저 생성
+browser = webdriver.Chrome('C:/chromedriver.exe')
+
+#웹사이트 열기
+browser.get('https://naver.com')
+browser.implicitly_wait(10) #로딩이 끝날 때, 10초까지는 기다려줌
+
+#쇼핑 메뉴 클릭하기
+browser.find_element_by_css_selector('a.nav.shop').click()
+time.sleep(2)
+
+#검색창 클릭
+search = browser.find_element_by_css_selector('input.co_srh_input._input')
+search.click()
+
+#검색어 입력
+search.send_keys('아이폰 13')
+search.send_keys(Keys.ENTER)
+
