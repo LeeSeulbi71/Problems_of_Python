f5b09fec4a312f879fb4d06d63a3627010f9a267
LeeSeulbi71 <dltmfql01@seoultech.ac.kr>
2021-11-06 16:29:53 +0900

    수정

--- 프로그래머스/프로그래머스_3진법 뒤집기.py
+++ 프로그래머스/프로그래머스_3진법 뒤집기.py
@@ -5,7 +5,7 @@ def solution(n):
         list_10_3.append(n%3)
         n = n//3
     list_10_3.extend([n%3, n//3])
-    print(list_10_3)
+
     for i in range(len(list_10_3)):
         answer += list_10_3[i]*(pow(3, len(list_10_3)-1-i))
     
