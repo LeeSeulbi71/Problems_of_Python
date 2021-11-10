20803ba41c8b49c70b9c3d7984e197b4eeb7f66a
LeeSeulbi71 <dltmfql01@seoultech.ac.kr>
2021-11-10 13:26:49 +0900

    백준

---
+++ 백준/백준_10103.py
@@ -0,0 +1,21 @@
+# 주사위 게임
+
+# 처음 점수: 100점
+# 낮은 숫자 = 높은 숫자만큼 점수를 잃음
+# 같을 때 = 점수를 잃지 않음
+
+Round_Num = int(input())
+Changyeong = 100
+Sangdeok = 100
+
+for i in range(Round_Num):
+    Changyeong_N, Sangdeok_N = map(int, input().split())
+    if Changyeong_N > Sangdeok_N:
+        Sangdeok -= Changyeong_N
+    elif Changyeong_N < Sangdeok_N:
+        Changyeong -= Sangdeok_N 
+    else:
+        continue
+
+print(f"{Changyeong} \n {Sangdeok}")
+        
--- 백준/백준_1934.py
+++ 백준/백준_1934.py
@@ -2,10 +2,24 @@
 
 Test_Num = int(input())
 
-for i in Test_Num:
+for i in range(Test_Num):
     A, B = map(int,input().split())
 
     for i in range(min(A,B), A*B+1):
         if i%A == 0 and i%B == 0:
             print(i)
             break
+# 이렇게 풀면 시간초과!
+# -> 유클리드 호제법으로 풀어야 함!
+
+T = int(input())
+for i in range(T):
+    A, B = map(int, input().split())
+    print(lcm(A, B))
+    
+def gcd(a, b):
+    return gcd(b%a, a) if b%a else a
+
+def lcm(a, b):
+    d = gcd(a, b)
+    return d * (a//d) * (b //d)
